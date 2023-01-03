9487b81bc1fe1c0669962ae45207e9bd822f0707
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-01-03 23:06:15 +0700

    ini useEffect and useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import {useEffect, useState} from 'react';
 
 function App() {
 
    const fetchPupolar = async() => {
     const data = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=f2e349fe9e8ecced437343534e51774f&language=en-US&page=1')
-   }
+    const movies = await data.json()
+
+  }
 
   return (
     <div className="App">
