148e7647545515612c11679ed9d54a1a76f59478
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-01-03 23:25:18 +0700

    finishing create useState and useEffect for fetch API

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import {useEffect, useState} from 'react';
 
 function App() {
 
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+        fetchPupolar();
+  } ,[]);
+
    const fetchPupolar = async() => {
     const data = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=f2e349fe9e8ecced437343534e51774f&language=en-US&page=1')
     const movies = await data.json()
-
+    setPopular(movies.results)
   }
 
   return (
     <div className="App">
-        <h1>Hello</h1>   
+          
     </div>
   );
 }
