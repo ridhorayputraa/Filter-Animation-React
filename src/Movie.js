fab2aae15cb95b394fe107970d21ba5ab9256dca
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-01-04 17:00:12 +0700

    create movies components

--- src/Movie.js
+++ src/Movie.js
@@ -2,7 +2,7 @@ function Movie({movie}){
     return(
         <div>
             <h2>{movie.title}</h2>
-            <img src="" alt="" />
+            <img src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} alt="" />
         </div>
     )
 }
